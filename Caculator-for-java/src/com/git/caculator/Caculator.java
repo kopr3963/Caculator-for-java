dbe2534be5ca491e9bbd73af855ca5aa20617559
김창회 <kopr3963@naver.com>
2016-12-19 21:29:58 +0900

    입력

---
+++ Caculator-for-java/src/com/git/caculator/Caculator.java
@@ -0,0 +1,26 @@
+package com.git.caculator;
+
+import java.util.Scanner;
+
+public class Caculator {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("계산기");
+		Scanner scanner = new Scanner(System.in);
+		
+		String calculation = scanner.nextLine();
+		
+		System.out.println(calculation);
+		System.out.println(calculation.indexOf("+"));
+		
+		
+//		int number = scanner.nextInt();
+		
+		
+		
+		
+	}
+}
