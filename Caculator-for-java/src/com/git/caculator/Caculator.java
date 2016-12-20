6ace4bad87ddeb9cdade6632ed0b188ff9c51bc6
김창회 <kopr3963@naver.com>
2016-12-20 21:44:18 +0900

    입력부 마이너스 테스트커밋

--- Caculator-for-java/src/com/git/caculator/Caculator.java
+++ Caculator-for-java/src/com/git/caculator/Caculator.java
@@ -4,23 +4,36 @@ import java.util.Scanner;
 
 public class Caculator {
 
-	
+	static int number1;
+	static int number2;
+	static int result = 0;
+
+	public Caculator() {
+	}
+
 	public static void main(String[] args) {
-		
-		
+
+		Subtraction subtraction = new Subtraction();
+
 		System.out.println("계산기");
 		Scanner scanner = new Scanner(System.in);
-		
 		String calculation = scanner.nextLine();
+
 		
-		System.out.println(calculation);
-		System.out.println(calculation.indexOf("+"));
+		if (calculation.indexOf("-") != -1) {
+			number1 = Integer.parseInt(calculation.split("-")[0]);
+			number2 = Integer.parseInt(calculation.split("-")[1]);
+			subtraction.setNumber(number1, number2);
+			result = subtraction.getSubtractionResult();
+			System.out.println(result);
+		}
 		
 		
-//		int number = scanner.nextInt();
-		
-		
-		
-		
+
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
 	}
 }
--- Caculator-for-java/src/com/git/caculator/Subtraction.java
+++ Caculator-for-java/src/com/git/caculator/Subtraction.java
@@ -12,37 +12,15 @@ public class Subtraction {
 		System.out.println("빼기연산을 하는 클래스.");
 	}
 	
+	
+	public void setNumber(int num1, int num2){
+		this. result = num1 - num2;
+	}
 	/*
 	 * 배열로 넣고 싶을 때 사용하세요.
 	 */
-	void setNumberArray(int[] numbers){
-		int size = numbers.length;
-		int temp = 0;
-		
-		for (int i =0;  i < size; i++ ){
-//			result = result - numbers[i];
-//			temp = numbers[i];
-//			result = temp - temp;
-//			temp = numbers[i] - numbers[i];
-			
-//			temp = numbers[i];
-			System.out.println(numbers[i]);
-		}
-		this.result = result;
-	}
-	
-	
 	public int getSubtractionResult(){
-		
 		return this.result;
 	}
-	public static void main(String[] args) {
-		
-		int[] numbers = {2,5,7};
-		
-		Subtraction subtraction = new Subtraction();
-		subtraction.setNumberArray(numbers);
-		System.out.println(2-5-7);
-		System.out.println(subtraction.getSubtractionResult());
-	}
+	
 }
